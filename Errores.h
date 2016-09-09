05b4a92e0832f160d936ac958ca761e58ee3e344
Nestor Aldana <nestor.aldana1@gmail.com>
2016-09-09 15:24:02 -0600

    Creación de archivo de cabecera para los mensajes de error

---
+++ Errores.h
@@ -0,0 +1,15 @@
+#ifndef ERRORES_H
+#define ERRORES_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+    char *errores[4] = {
+        "Error el archivo no pudo ser abierto","","","",""
+    };
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* ERRORES_H */
