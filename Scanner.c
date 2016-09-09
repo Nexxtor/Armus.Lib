27ed52776ee9ed4b66f8294e58de60ad201a82af
Nestor Aldana <nestor.aldana1@gmail.com>
2016-09-09 12:33:04 -0600

    Copiando String

--- Scanner.c
+++ Scanner.c
@@ -1,10 +1,23 @@
 #include <jni.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #include "Scanner.h"
+
+#define MAX_FILE 150
+#define MAX_NAME_FILE 256
 
 JNIEXPORT jobjectArray JNICALL Java_armus_lib_scanner_Scanner_lsFiles
   (JNIEnv *env, jobject obt, jstring pathFirtsFile){
     const char *strFirstFile = (*env)->GetStringUTFChars(env,pathFirtsFile,NULL);
-    printf("%s", strFirstFile);
+        
+    char **lsArchivos = (char **) malloc(MAX_FILE * sizeof(char *) );
+    lsArchivos[0] = (char *) malloc(MAX_NAME_FILE * sizeof(char) );
+    
+    strncpy(lsArchivos[0], strFirstFile, MAX_NAME_FILE );
+    
+    printf("%s\n", lsArchivos[0]);
+    
     return NULL;                      
 }
+
