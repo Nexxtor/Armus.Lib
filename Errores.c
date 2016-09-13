6e5626304506187c201f9856a7cbfbdca03504c3
Nestor Aldana <nestor.aldana1@gmail.com>
2016-09-13 16:16:06 -0600

    Agregado extern int primerError

--- Errores.c
+++ Errores.c
@@ -1,10 +1,17 @@
 #include <stdio.h>
 #include "Errores.h"
 
-char *errores[5] = {"Error el archivo no pudo ser abierto", "", "", "", ""};
+char *errores[5] = {"Error el archivo no pudo ser abierto\n", "", "", "", ""};
+int primerError = 0;
 
 void log_error(int error){
-    FILE *f=fopen("log.txt","a");
+    FILE *f;
+    if(primerError == 0){
+        f=fopen("log.txt","w");
+        primerError = 1;
+    }else{
+        f=fopen("log.txt","a");
+    }
     if(f==NULL){
         printf("Verifique los permisos sobre el directorio\n");
         return;
