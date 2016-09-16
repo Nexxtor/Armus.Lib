293f14439f77c1d45675972998fc11848e1768a0
Nestor Aldana <nestor.aldana1@gmail.com>
2016-09-16 16:42:34 -0600

    Definición correcta en el arreglo errores, con los mensajes finales que se imprimen en el archivo log.txt, numerados de 000 a 006

--- Errores.c
+++ Errores.c
@@ -1,7 +1,14 @@
 #include <stdio.h>
 #include "Errores.h"
 
-char *errores[5] = {"Error el archivo no pudo ser abierto\n", "", "", "", ""};
+char *errores[7] = {
+    "000 Error importante: un archivo no pudo ser abierto \n",
+    "001 Error: existe un enter entre incluir y \" \n",
+    "002 Error: falto comillia (\") de cierre en un incluir \n",
+    "003 Error: falto un ; al final de una declaración incluir \n",
+    "004 Error: salto de linea inesperado en una declaración incluir \n",
+    "005 Error: nombre o ruta del archivo es demasiado largo \n",
+    "006 Error importante: se alcanzo la cantidad maxima de archivos a incluir \n"};
 int primerError = 0;
 
 void log_error(int error){
