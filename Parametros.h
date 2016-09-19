c0e60ffe8cffc9cd94718a725da0efbef78d4544
Nestor Aldana <nestor.aldana1@gmail.com>
2016-09-19 10:19:23 -0600

    Agregados todos los define de los archivos

---
+++ Parametros.h
@@ -0,0 +1,21 @@
+#ifndef PARAMETROS_H
+#define PARAMETROS_H
+#define MAX_LINEA 1024
+#define MAX_ID 1024
+#define MAX_DIGIT 20
+#define MAX_FILE 150
+#define MAX_NAME_FILE 256*4
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* PARAMETROS_H */
+
