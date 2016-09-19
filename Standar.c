28c48a7470a73b925a3595d6f0ed147d9de3d35c
Nestor Aldana <nestor.aldana1@gmail.com>
2016-09-19 15:05:14 -0600

    agregadas las funciones obtoken

--- Standar.c
+++ Standar.c
@@ -1,10 +1,20 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
 #include "Standar.h"
 #include "Lexico.h"
+#include "Errores.h"
 
 FILE *fp = NULL;
-char linea[MAX_LINEA];
+char linea[MAX_LINEA];     //buffer de líneas 
+int ll;                   //contador de caracteres
+int offset;               //corrimiento en la lectura de los caracteres del programa fuente
+int fin_de_archivo;       //bandera de fin de archivo (obtch)   
+int ch;                   //último caracter leído
+char lex[MAX_ID+1];        //último lexeme leído ( +1 para colocar "\0")
+long int valor ;          //valor numérico de una lexeme correspondiene a un número
+
 
 //getline: lee la siguiente línea del fuente y regresa su tamaño (incluyendo '\n') o 0 si EOF. (por ejemplo: para VAR regresa 4)
 //es probablemente la rutina de más bajo nivel del compilador
@@ -67,4 +77,122 @@ void imprime_token()
  printf("(%10s) \n",token_string[token]);
   
 */
+}
+
+//obtoken: obtiene el siguiente token del programa fuente                   
+void obtoken()
+{
+ char lexid[MAX_ID+1]; //+1 para colocar el marcador "\0"
+ int i,j;
+ int ok=0;
+
+ //quitar blancos, caracter de cambio de línea y tabuladores
+ while (ch==' ' || ch=='\n' || ch=='\t') ch=obtch() ;
+
+ //si la lexeme comienza con una letra, es identificador o palabra reservada
+ if (isalpha(ch)) {
+    lexid[0]=ch;
+    i=1;
+    while ( isalpha( (ch=obtch()) ) ||  isdigit(ch)   ) 
+      if (i<MAX_ID) lexid[i++]=ch;
+    lexid[i]='\0';
+  
+    //¿es identificador o palabra reservada?.buscar en la tabla de palabras reservadas
+	//una búsqueda lineal que tendrá que ser sustituída por otro tipo de búsqueda más efectiva. 
+	//...en esa nueva búsqueda desaparecerá el "break"
+    for (j=0;j<MAXPAL;++j) 
+        if (strcmp(lexid,lexpal[j])==0) {
+	       ok=1;
+	       break;
+        }
+
+    if (ok==1) 
+       token=tokpal[j]; //es palabra reservada
+    else
+       token=ident; //es identificador
+ 
+    strcpy(lex,lexid); //copiar en lex
+ }
+ else //si comienza con un dígito...
+    if (isdigit(ch)) {
+       lexid[0]=ch;
+       i=j=1;
+       while ( isdigit( (ch=obtch()))) {
+	         if (i<MAX_DIGIT) lexid[i++]=ch;
+	         j++;
+       }
+       lexid[i]='\0';
+       if (j>MAX_DIGIT)
+          log_error(30); //este número es demasiado grande
+       token=numero;
+       valor=atol(lexid); //valor numérico de una lexeme correspondiene a un número	        
+    }
+    else //reconocimiento de símbolos especiales, incluyendo pares de simbolos (aplicamos "lookahead symbol technique")
+       if (ch=='<') {
+          ch=obtch();
+          if (ch=='=') {
+             token=mei;
+             ch=obtch();
+          }
+          else
+             if (ch=='>') {
+                token=nig;
+                ch=obtch();
+             }
+             else
+                token=mnr;
+       }
+       else
+          if (ch=='>') {
+             ch=obtch();
+             if (ch=='=') {
+                token=mai;
+                ch=obtch();
+             }
+             else 
+                token=myr;
+          }
+          else 
+             if (ch==':') {
+                ch=obtch();
+                if (ch=='=') {
+	               token=asignacion;
+	               ch=obtch();
+                }
+               else
+	               token=nulo;
+             }
+             else {
+                token=espec[ch]; //hashing directo en la tabla de tokens de símbolos especiales del lenguaje
+                ch=obtch();
+             }
+}
+
+//obtch: obtiene el siguiente caracter del programa fuente
+int obtch()
+{ 
+
+ if (fin_de_archivo==1) {
+	fclose(fp);//cerrar el programa fuente
+    printf("Analisis lexicografico finalizado.");
+    exit(1); //salir...
+ }
+  
+ if (offset==ll-1) {
+    ll=getLine(linea,MAX_LINEA); //trae una línea del programa fuente al buffer de líneas
+    if (ll==0) 	   
+       fin_de_archivo=1; //se retrasa en un blanco la deteccion de EOF, porque obtoken lleva un caracter adelantado. si no, en 
+						 //algunos casos tendríamos problemas, por ejemplo: no se reconoceria el punto final del programa (...end.)
+
+    printf("\n%s",linea);
+    offset=-1;
+ }
+
+ ++offset;
+
+ if ( (linea[offset]=='\0') || (fin_de_archivo==1) )   
+    return(' '); 
+ else  
+    return(toupper(linea[offset])); //de esto depende si el lenguaje es sensitivo de mayúsculas o no.
+
 }
