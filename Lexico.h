7ed448204cee90a81b90c5b42cc1c0ec542c46c5
Nestor Aldana <nestor.aldana1@gmail.com>
2016-09-19 14:26:15 -0600

    se cambio y por ytok y o por otok

--- Lexico.h
+++ Lexico.h
@@ -10,8 +10,8 @@ extern "C" {
 extern char *lexpal[MAXPAL];
 
 //lista de tokens de ARMUS
-enum simbolo {nulo,ident,numero,mas,menos,por,div,llaveI,llaveF,parentI,parentF,corcheteI,cortecheF,punto,coma,
-puntoycoma,asginacion,mei,mai,myr,mnr,igl,nig,negacion,y,o,hexaTok,referencia,enteroTok,byteTok,
+enum simbolo {nulo,ident,numero,mas,menos,por,barra,llaveI,llaveF,parentI,parentF,corcheteI,corcheteF,punto,coma,
+puntoycoma,asginacion,mei,mai,myr,mnr,igl,nig,negacion,ytok,otok,hexaTok,referencia,enteroTok,byteTok,
 realTok,vacioTok,booleanoTok,cadenaTok,caracterTok,objetoTok,archivoTok,siTok,sinoTok,probarTok,casoTok,defectoTok,
 romperTok,mientrasTok,paraTok,hacerTok,paracadaTok,sistemaTok,obtenerEnteroTok,obtenerRealTok,obtenerCadenaTok,
 obtenerCaracterTok,mostrarTok,publicaTok,privadaTok,retornarTok,arregloTok,agregarTok,obtenerTok,cuantosTok,
