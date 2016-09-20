a2fd064be6908b31f6020d2c9cf4501ebe44acfc
Nestor Aldana <nestor.aldana1@gmail.com>
2016-09-19 21:37:50 -0600

    tokenisar un archivo

--- Scanner.c
+++ Scanner.c
@@ -6,9 +6,34 @@
 #include "Errores.h"
 #include "Standar.h"
 #include "Scanner.h"
+#include "Lexico.h"
 
 JNIEXPORT jobjectArray JNICALL Java_armus_lib_scanner_Scanner_lsTokens
-  (JNIEnv *env, jobject obj, jobjectArray allFiles){
-     printf("Hola mundo");
-     return NULL;
- }
+(JNIEnv *env, jobject obj, jobjectArray allFiles) {
+    int cant = 1;
+
+    while (cant > 0) {
+        fp = fopen("/home/nextor/Escritorio/proyecto/main.acl", "r"); //abrir el fuente solo para lectura
+
+        printf("\n\nCompilador de armus version 0.1\n");
+        //inicializacion de tokens de símbolos especiales 
+        inicializar_espec();
+        inicializarArbolPalabras();
+
+        //inicializacion de otras variables 
+        ch = ' ';
+        fin_de_archivo = 0;
+        offset = -1;
+        ll = 0;
+
+        //tokenizar el programa fuente
+        while (1) {
+            obtoken();
+            imprime_token(); 
+        }
+        
+        cant--;
+
+    }
+    return NULL;
+}
