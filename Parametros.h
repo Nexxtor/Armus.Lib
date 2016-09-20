b49bc57c8a5d81aeb69cd0431b5e3e48d95a9982
Nestor Aldana <nestor.aldana1@gmail.com>
2016-09-19 21:38:15 -0600

    Tamaño maximo de cadena

--- Parametros.h
+++ Parametros.h
@@ -5,7 +5,9 @@
 #define MAX_DIGIT 20
 #define MAX_FILE 150
 #define MAX_NAME_FILE 256*4
-#define MAXPAL 58  //numero de palabras reservadas
+#define MAXPAL 53  //numero de palabras reservadas
+#define MAX_CADENA 123456
+
 
 #ifdef __cplusplus
 extern "C" {
