ecf7842a5b9b44abca5cddee5fe08fca04d03da4
Nestor Aldana <nestor.aldana1@gmail.com>
2016-09-19 21:36:46 -0600

    añadido arbol para busqueda binario

--- Lexico.c
+++ Lexico.c
@@ -1,27 +1,87 @@
+#include <stddef.h>
+#include <stdio.h>
+
 #include "Lexico.h"
 
 //se define e inicializa la tabla de lexemes correspondientes a las palabras reservadas
-char *lexpal[MAXPAL]={"si","byte","real","sino","caso","para","vacio","hacer",
-"abrir","mayor","menor","esPar","clase","falso","entero","cadena","Objeto",
-"probar","romper","quitar","cerrar","modulo","Archivo","defecto","Sistema",
-"mostrar","publica","privada","Arreglo","agregar","obtener","cuantos","volcado",
-"incluir","booleano","caracter","mientras","retornar","comparar","absoluto",
-"paraCada","leerLinea","potencia","verdadero","concatenar","decimalBin",
-"obtenerReal","parteEntera","obtenerEntero","ObtenerCadena","longitudCadena",
-"obtenerCaracter","obtenerBooleano"};
-
+arbol *lexpal;
 //el token
 enum simbolo token;
 
 //se define e inicializa la tabla de tokens de palabras reservadas
-enum simbolo tokpal[MAXPAL]={siTok,byteTok,realTok,sinoTok,casoTok,paraTok,vacioTok,hacerTok,
-abrirTok,mayorTok,menorTok,esParTok,claseTok,falsoTok,enteroTok,cadenaTok,objetoTok,
-probarTok,romperTok,quitarTok,cerrarTok,moduloTok,archivoTok,defectoTok,sistemaTok,
-mostrarTok,publicaTok,privadaTok,arregloTok,agregarTok,obtenerTok,cuantosTok,volcadoTok,
-incluirTok,booleanoTok,caracterTok,mientrasTok,retornarTok,compararTok,absolutoTok,
-paracadaTok,leerLineaTok,potenciaTok,verdaderoTok,concatenarTok,decimalBinTok,
-obtenerRealTok,parteEnteraTok,obtenerEnteroTok,obtenerCadenaTok,longitudCadenaTok,
-obtenerCaracterTok,obtenerBooleanoTok};
+enum simbolo tokpal[MAXPAL] = {siTok, byteTok, realTok, sinoTok, casoTok, paraTok, vacioTok, hacerTok,
+    abrirTok, mayorTok, menorTok, esParTok, claseTok, falsoTok, enteroTok, cadenaTok, objetoTok,
+    probarTok, romperTok, quitarTok, cerrarTok, moduloTok, archivoTok, defectoTok, sistemaTok,
+    mostrarTok, publicaTok, privadaTok, arregloTok, agregarTok, obtenerTok, cuantosTok, volcadoTok,
+    incluirTok, booleanoTok, caracterTok, mientrasTok, retornarTok, compararTok, absolutoTok,
+    paracadaTok, leerLineaTok, potenciaTok, verdaderoTok, concatenarTok, decimalBinTok,
+    obtenerRealTok, parteEnteraTok, obtenerEnteroTok, obtenerCadenaTok, longitudCadenaTok,
+    obtenerCaracterTok, obtenerBooleanoTok};
 
 //tabla de tokens correspondientes a operadores y símbolos especiales
-enum simbolo espec[255] ;
+enum simbolo espec[255];
+
+void inicializarArbolPalabras() {
+    char *lexPalAux[MAXPAL] = {"si", "byte", "real", "sino", "caso", "para", "vacio", "hacer",
+        "abrir", "mayor", "menor", "esPar", "clase", "falso", "entero", "cadena", "Objeto",
+        "probar", "romper", "quitar", "cerrar", "modulo", "Archivo", "defecto", "Sistema",
+        "mostrar", "publica", "privada", "Arreglo", "agregar", "obtener", "cuantos", "volcado",
+        "incluir", "booleano", "caracter", "mientras", "retornar", "comparar", "absoluto",
+        "paraCada", "leerLinea", "potencia", "verdadero", "concatenar", "decimalBin",
+        "obtenerReal", "parteEntera", "obtenerEntero", "ObtenerCadena", "longitudCadena",
+        "obtenerCaracter", "obtenerBooleano"};
+
+    int mitad = MAXPAL / 2, i = 0;
+
+    arbol* raiz = NULL;
+    raiz = (arbol *) malloc(sizeof (arbol));
+
+    raiz->value = sumCadena(lexPalAux[mitad]);
+    raiz->derecha = NULL;
+    raiz->izq = NULL;
+
+    for (i = 0; i < MAXPAL; i++) {
+        if (mitad != i) {
+            printf("%d %s\n",i,lexPalAux[i]);
+            insertarArbol(sumCadena(lexPalAux[i]), &raiz, i);
+        }
+    }
+    lexpal = raiz;
+
+}
+
+int sumCadena(char *cad) {
+    int suma = 0, i = 0;
+    int larg = strlen(cad);
+    for(i = 0 ; i< larg ; i ++){
+        suma += cad[i];
+    }
+    return suma;
+}
+
+void insertarArbol(int valor, arbol **raiz, int pos) {
+    arbol *nuevo;
+    if (valor > (*raiz)->value) {
+        if ((*raiz)->derecha == NULL) {
+            nuevo = (arbol *) malloc(sizeof (arbol));
+            nuevo->value = valor;
+            nuevo->derecha = NULL;
+            nuevo->izq = NULL;
+            nuevo->token = tokpal[pos];
+            (*raiz)->derecha =nuevo;
+        } else {
+            insertarArbol(valor, &((*raiz)->derecha),pos);
+        }
+    } else {
+        if ((*raiz)->izq == NULL) {
+            nuevo = (arbol *) malloc(sizeof (arbol));
+            nuevo->value = valor;
+            nuevo->derecha = NULL;
+            nuevo->izq = NULL;
+            (*raiz)->izq =nuevo;
+            nuevo->token = tokpal[pos];
+        } else {
+            insertarArbol(valor, &((*raiz)->izq),pos);
+        }
+    }
+}
